0fefb1092cb80232f3746406e2ec926f0b48e3d1
FABIO SANTOS DE MENDONÇA <Fabio.Mendonca@jotanunes.net>
2020-08-05 09:24:32 -0300

    atualizacao para depoisflix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
+
+
+function erro404()  {
+  return(
+    <div>
+      PAGINA NÃO ENCONTRADA.
+      ERRO 404.
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />      
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />  
+      <Route component={erro404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
